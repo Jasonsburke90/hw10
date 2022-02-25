1382c306452050da443c1b1c0c27f0029870f19d
unknown <jasonsburke90@gmail.com>
2022-02-24 21:32:44 -0600

    got engineer and intern prompts added

--- Main/index.js
+++ Main/index.js
@@ -44,12 +44,64 @@ function createManager() {
       );
       employees.push(manager);
       console.log(employees);
+      createEmployee();
     });
-  // call function createEmployee
 }
 
 // add function createEmployee, with new inquirer request
+function createEmployee() {
+  inquirer.prompt([
+    {
+      name: "role",
+      type: "list",
+      choices: ["engineer", "intern"],
+      message: "select employee's role",
+    },
 
+    {
+      name: "name",
+      type: "input",
+      message: "Enter employee's name",
+    },
+
+    {
+      name: "id",
+      type: "input",
+      message: "Enter manager's ID #",
+    },
+
+    {
+      name: "email",
+      type: "input",
+      message: "Enter employee's email address",
+    },
+
+    {
+      name: "github",
+      type: "input",
+      when: (answers) => answers.role === "engineer",
+      message: "Enter engineers's github profile name",
+    },
+
+    {
+      name: "school",
+      type: "input",
+      when: (answers) => answers.role === "intern",
+      message: "Enter intern's school's name",
+    },
+  ]);
+  // set up this based on whether eng or intern is selected
+  // .then((answer) => {
+  //   const manager = new Manager(
+  //     answer.name,
+  //     answer.id,
+  //     answer.email,
+  //     answer.officeNumber
+  //   );
+  //   //  set up employees push to engineer OR intern based on response above
+  //   console.log(employees);
+  // });
+}
 // do you want to add more employees?
 // do you want to add an engineer or an intern
 // add inquirer prompts for engineers and interns
